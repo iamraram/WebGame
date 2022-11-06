ab7db3a7f35442473ad057a23519cd07b4577838
Haram Lee <74528522+iamraram@users.noreply.github.com>
2022-11-06 15:36:51 +0900

    commit

--- cookieRun/index.js
+++ cookieRun/index.js
@@ -34,7 +34,7 @@ class Cookie {
         this.h = h
 
         this.dy = 0
-        this.jumpForce = 10
+        this.jumpForce = 15
         this.originalHeight = h
         this.grounded = false
         this.jumpTimer = 0
@@ -43,7 +43,7 @@ class Cookie {
     Draw() {
         let img = new Image()
 
-        if (keys['KeyJ'] && this.grounded) {   
+        if (!this.grounded) {   
             img.src = 'cookie_jump.png'
             ctx.drawImage(img, this.x, this.y, this.w, this.h)
         }
@@ -101,20 +101,18 @@ class Cookie {
 }
 
 class Obstacle {
-    constructor (x, y, w, h, is) {
+    constructor (x, y, w, h) {
         this.x = x
         this.y = y
         this.w = w
         this.h = h
-        this.is = is
 
         this.dx = -gameSpeed
-        this.isBird = false
     }
 
     Draw() {
         let img = new Image()
-        img.src = 'jelly.png'
+        img.src = 'frog.png'
         ctx.drawImage(img, this.x, this.y, this.w, this.h);
     }
 
@@ -127,12 +125,11 @@ class Obstacle {
 
 class Jelly {
     constructor (x, y, w, h, isTouched) {
-        // 여기에 is를 줘서 닿으면 true 안 닿으면 false 한 다음에
-        // 화면 밖으로 나가서 소멸될 때 콤보 체크하는 걸로 했는데 그게 안되는 것 같아요
         this.x = x
         this.y = y
         this.w = w
         this.h = h
+        this.isTouched = isTouched
 
         this.dx = -gameSpeed
         this.isBird = false
@@ -183,12 +180,25 @@ const SpawnJelly = () => {
     let jelly = new Jelly(canvas.width + size, canvas.height - size, size, size)
   
     if (type == 1) {
-        jelly.y -= cookie.originalHeight + 85
+        jelly.y -= cookie.originalHeight + 200
     }
     else if (type == 2) {
-        jelly.y -= cookie.originalHeight + 15
+        jelly.y -= cookie.originalHeight + 30
     }
     jellies.push(jelly)
+}
+
+const SpawnObstacle = () => {
+    size = 70
+    let type = RandomIntInRange(0, 1)
+
+    let obstacle = new Obstacle(canvas.width + size, canvas.height - size, size, size)
+
+    if (type == 1) {
+        obstacle.y -= cookie.originalHeight - 10
+    }
+    obstacles.push(obstacle)
+    console.log(obstacles)
 }
 
 const RandomIntInRange = (min, max) => {
@@ -202,7 +212,7 @@ const Start = () => {
   
     ctx.font = "20px sans-serif"
   
-    gameSpeed = 1
+    gameSpeed = 0.5
     gravity = 1
 
     score = 0
@@ -212,7 +222,7 @@ const Start = () => {
     scoreText = new Text("현재 점수: " + score, 35, 50, "left", "white", "35")
     comboText = new Text("콤보: " + combo, 35, 80, "left", "white", "20")
 
-    cookie = new Cookie(25, canvas.height-150, 100, 100)
+    cookie = new Cookie(25, canvas.height-150, 80, 80)
 
     requestAnimationFrame(Update)
 }
@@ -220,12 +230,17 @@ const Start = () => {
 let initialSpawnTimer = 1
 let spawnTimer = initialSpawnTimer
 
+let O_initialSpawnTimer = 1
+let O_spawnTimer = O_initialSpawnTimer
+
 const init = () => {
     jellies = []
-    gameSpeed = 3
+    obstacles = []
+    gameSpeed = 5
     score = 0
     combo = 0
     spawnTimer = initialSpawnTimer
+    O_spawnTimer = O_initialSpawnTimer
 }
 
 const Update = () => {
@@ -234,19 +249,47 @@ const Update = () => {
     cookie.Animate()
 
     spawnTimer --
+    O_spawnTimer --
 
     if (spawnTimer <= 0) {
         SpawnJelly()
         spawnTimer = initialSpawnTimer - gameSpeed * 8
         
         if (spawnTimer < 30) {
-        spawnTimer = 30
+            spawnTimer = 30
         }
+    }
+
+    if (O_spawnTimer <= 0) {
+        SpawnObstacle()
+        O_spawnTimer = O_initialSpawnTimer - gameSpeed * 8
+        
+        if (O_spawnTimer < 320) {
+            O_spawnTimer = 320
+        }
+    }
+
+    for (let i = 0; i < obstacles.length; i ++) {
+        let o_obstacle = obstacles[i]
+        o_obstacle.isTouched = false
+
+        if (o_obstacle.x + o_obstacle.w < 0) {
+            obstacles.splice(i, 1)
+        }
+        if (
+            cookie.x < o_obstacle.x + o_obstacle.w &&
+            cookie.x + cookie.w > o_obstacle.x &&
+            cookie.y < o_obstacle.y + o_obstacle.h &&
+            cookie.y + cookie.h > o_obstacle.y
+            ) {
+                location.reload()
+        }
+
+        o_obstacle.Update()
     }
 
     for (let i = 0; i < jellies.length; i ++) {
         let o = jellies[i]
-        o.isTouched = false
 
         if (o.x + o.w < 0) {
             jellies.splice(i, 1)
@@ -261,23 +304,23 @@ const Update = () => {
             cookie.y < o.y + o.h &&
             cookie.y + cookie.h > o.y
             ) && (o.isTouched == false)) {
-            o.w = 0
-            o.h = 0
-            combo += 1
-            score += (17 + (2 * (combo + 1)))
-            isTouched = true
+                jellies.splice(i, 1)
+                combo += 1
+                score += (17 + (2 * (combo + 1)))
+                isTouched = true
         }
 
         o.Update()
     }
 
     let result = score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
     scoreText.t = "현재 점수: " + result
     comboText.t = "콤보: " + combo
     scoreText.Draw()
     comboText.Draw()
 
-    gameSpeed += 0.001;
+    gameSpeed += 0.007;
 }
 
 const getJson = (url, callback) => {
